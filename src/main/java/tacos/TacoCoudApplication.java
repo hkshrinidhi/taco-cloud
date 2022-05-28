fab5df6e4c0b1d41791adbf5f8f62b114df99a67
Shrinidhi Kulkarni <shrinidhihk456@gmail.com>
2022-05-28 18:23:51 +0530

    Intial Commit

---
+++ src/main/java/tacos/TacoCoudApplication.java
@@ -0,0 +1,13 @@
+package tacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCoudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoCoudApplication.class, args);
+	}
+
+}
